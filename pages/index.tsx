dd138fbe0fd237112bb6d896cbc1134879d11085
52617365 <rasengithub@protonmail.com>
2022-12-07 15:05:37 +0200

    changed icon to a nice smiley.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import {RuokalistatResponse, SetMenu, Meal} from "../types/ruokalistat";
+import {RuokalistatResponse} from "../types/ruokalistat";
 import RestaurantCard from "../components/RestaurantCard";
 export default function Home() {
-    // send request to backend
     const [data, setData] = React.useState<Array<RuokalistatResponse>>([]);
     React.useEffect(() => {
         fetch("/api/ruokalistat")
@@ -13,7 +12,6 @@ export default function Home() {
   return (
       <>
         <h1 className={"text-center text-2xl font-mono mt-28"}>Mitä tänään on ruokana?</h1>
-          {/* center grid */}
             <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center"}>
               {renderFoodsFromRestaurants(data)}
           </div>
