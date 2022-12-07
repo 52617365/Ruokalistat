62712ba401eeace469831b30075f8ea9e1bb3f67
52617365 <rasengithub@protonmail.com>
2022-12-07 12:52:00 +0200

    Repo init with next.js, tailwind, daisyui.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    themes: ["dark"],
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
