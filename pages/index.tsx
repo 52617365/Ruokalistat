848244c01a7e2409fc61fe45f39c81f62a03f6e6
52617365 <rasengithub@protonmail.com>
2022-12-07 15:12:35 +0200

    changed the grids a bit.

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
       <>
         <h1 className={"text-center text-2xl font-mono mt-28"}>Mitä tänään on ruokana?</h1>
-            <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center"}>
+            <div className={"grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 justify-items-center"}>
               {renderFoodsFromRestaurants(data)}
           </div>
       </>
