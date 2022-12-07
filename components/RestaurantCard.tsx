ca8c579b50035549d735c6a28fe3fdd23a696a75
52617365 <rasengithub@protonmail.com>
2022-12-07 17:59:25 +0200

    got rid of shadow.

--- components/RestaurantCard.tsx
+++ components/RestaurantCard.tsx
@@ -3,7 +3,7 @@ import {RuokalistatResponse, SetMenu} from "../types/ruokalistat";
 import SetMenuCard from "./SetMenuCard";
 export default function RestaurantCard({restaurant} : {restaurant: RuokalistatResponse}) {
     return (
-        <div className="mt-8 w-full card w-96 shadow-xl">
+        <div className="mt-8 card w-96">
             <div className="card-body">
                 <h2 className="card-title"><strong>Ravintola:</strong> {restaurant.LunchMenu.Restaurant}</h2>
                 {renderSetMenus(restaurant.LunchMenu.SetMenus)}
